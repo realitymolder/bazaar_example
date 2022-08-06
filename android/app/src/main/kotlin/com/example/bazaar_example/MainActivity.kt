bff6ff83d8cf3f7fb9fae530dd5e4075a3d14228
Reality Molder <26459151+realitymolder@users.noreply.github.com>
2022-08-06 21:57:38 +0300

    added a flutter example app

---
+++ android/app/src/main/kotlin/com/example/bazaar_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bazaar_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ bazaar/description.md
@@ -0,0 +1,5 @@
+# This is a markdown file
+
+## Full of elements
+
+### Anoither line full of stuff lolz? this is our format..
